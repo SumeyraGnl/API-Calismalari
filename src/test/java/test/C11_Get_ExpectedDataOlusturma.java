34ff8c8a6743b28e1cb21102f2b7dfefa167d16e
SumeyraGnl <h.berengunel@gmail.com>
2023-06-19 20:56:38 +0300

    19.06.2023

---
+++ src/test/java/test/C11_Get_ExpectedDataOlusturma.java
@@ -0,0 +1,50 @@
+package test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C11_Get_ExpectedDataOlusturma {
+
+    /*
+    https://jsonplaceholder.typicode.com/posts/22 url'ine
+    bir GET request yolladigimizda donen response body’sinin
+    asagida verilen ile ayni oldugunu test ediniz
+   Response body :
+    {
+    "userId":3,
+    "id":22,
+    "title":"dolor sint quo a velit explicabo quia nam",
+    "body":"eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse"
+    }
+     */
+
+    @Test
+    public void get01(){
+
+        // URL hazırla
+
+        String url = "https://jsonplaceholder.typicode.com/posts/22";
+
+        // Expected data hazırla
+
+        JSONObject expData = new JSONObject();
+
+        expData.put("userId",3);
+        expData.put("id",22);
+        expData.put("title","dolor sint quo a velit explicabo quia nam");
+        expData.put("body","eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse");
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .body(expData.toString())
+                .put(url);
+
+        response.prettyPrint();
+    }
+
+}
